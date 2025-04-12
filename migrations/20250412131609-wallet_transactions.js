e0a7df40bda641e146643eee6a230361a5cb3a33
Akshay Malik <34898791+akshaykmalik@users.noreply.github.com>
2025-04-12 18:51:21 +0530

    feat : create wallet_transaction migration

---
+++ migrations/20250412131609-wallet_transactions.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('wallet_transactions', {
+      id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      transaction_amount : {
+        type: Sequelize.DOUBLE,
+        defaultValue: 0,
+        allowNull: false,
+      },
+      wallet_id: {
+        type: Sequelize.BIGINT,
+        allowNull : true,
+        references : {
+          model: {
+            tableName: 'user_wallet',
+            // schema: 'schema',
+          },
+          key: 'id',
+        },
+        onUpdate: 'CASCADE'
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      deleted_at : {
+        type : Sequelize.DATE,
+        defaultValue : null,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('wallet_transactions');
+  }
+};
