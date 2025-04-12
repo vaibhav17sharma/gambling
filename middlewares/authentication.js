4fed3026509bfe5db8513a1a6ea1503edf2fe152
Akshay Malik <34898791+akshaykmalik@users.noreply.github.com>
2025-04-12 13:42:57 +0530

    feat : create authentication middleware

---
+++ middlewares/authentication.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+async function authenticate(req, res, next) {
+    // Mock User Database
+    // Login Endpoint
+   
+    next();
+}
