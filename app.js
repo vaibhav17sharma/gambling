bbc4b2ea255efdc71e5cb9dd35e886b90ba25f89
Vaibhav Sharma <vaibhav17sharma.it@gmail.com>
2025-04-15 23:36:00 +0530

    cors and api wrap

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
 require('dotenv').config();
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var authenticateMiddleware=require('./middlewares/authentication');
+const createError = require('http-errors');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const authenticateMiddleware = require('./middlewares/authentication');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const authRouter = require('./routes/authentication');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', authRouter);
-app.use('/user',authenticateMiddleware, usersRouter);
+app.use('/api', indexRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/user', authenticateMiddleware, usersRouter);
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
 
-  // render the error page
+  if (req.originalUrl.startsWith('/api')) {
+    return res.json({
+      error: {
+        message: err.message,
+        status: err.status || 500,
+      },
+    });
+  }
+
   res.render('error', {
     title: 'Error',
     message: err.message,
-    error: err
+    error: req.app.get('env') === 'development' ? err : {},
   });
-
 });
 
 module.exports = app;
