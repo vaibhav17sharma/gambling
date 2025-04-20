bdb4b19a2c3e94e625d3524eff9a5ecdc0f3d9b9
Akshay Malik <34898791+akshaykmalik@users.noreply.github.com>
2025-04-20 20:26:00 +0530

    feat: add status to wallet_transactions

---
+++ migrations/20250420100820-alter-wallet-transactions-add-column-status.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('wallet_transactions', 'status', {
+      type: Sequelize.ENUM('approved', 'pending'),
+      allowNull: false,
+      defaultValue: 'pending',
+      after: 'type'
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('wallet_transactions', 'status');
+};
